852646280a2142307f4991d128d09dbc7702bf5c
simontcodes <simont.codes@gmail.com>
2024-06-04 15:44:28 -0500

    product module finished

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -21,6 +21,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
   value,
 }) => {
   const [isMounted, setIsMounted] = useState(false);
+  console.log(value);
 
   useEffect(() => {
     setIsMounted(true);
@@ -34,31 +35,37 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     return null;
   }
 
+  //BUG: only showing one image preview instead of all product images
   return (
     <div>
-      <div className=" mb-4 flex items-center gap-4">
+      
+      <div className="mb-4 flex items-center gap-4">
         {value.map((url) => (
           <div
             key={url}
-            className=" relative w-[200px] h-[200px] rounded-md overflow-hidden"
+            className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
           >
-            <div className=" z-10 absolute top-2 right-2">
+            <div className="z-10 absolute top-2 right-2">
               <Button
                 type="button"
                 onClick={() => onRemove(url)}
                 variant="destructive"
                 size="icon"
               >
-                <Trash className=" h-4 w-4" />
+                <Trash className="h-4 w-4" />
               </Button>
             </div>
-            <Image fill className=" object-cover" alt="image" src={url} />
+            <Image
+              fill
+              sizes=""
+              className="object-cover"
+              alt="image"
+              src={url}
+            />
           </div>
         ))}
       </div>
-      <CldUploadWidget uploadPreset="nm987wbf" 
-      onSuccess={onUpload}
-  >
+      <CldUploadWidget uploadPreset="nm987wbf" onSuccess={onUpload}>
         {({ open }) => {
           const onClick = () => {
             open();
@@ -71,7 +78,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
               variant="secondary"
               onClick={onClick}
             >
-              <ImagePlus className=" h-4 w-4 mr-2" />
+              <ImagePlus className="h-4 w-4 mr-2" />
               Upload an Image
             </Button>
           );
